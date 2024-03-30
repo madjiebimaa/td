8bb984ad0df2df3e23a942623d451ca815a0f3dd
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2024-03-30 20:14:11 +0700

    feat: add auto grow input component

---
+++ src/components/global/auto-grow-input.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+
+import { Textarea } from "@/components/ui/textarea";
+
+import { cn } from "@/lib/utils";
+
+interface AutoGrowInputProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const AutoGrowInput = React.forwardRef<HTMLTextAreaElement, AutoGrowInputProps>(
+  ({ className, ...props }, ref) => {
+    const handleInputGrow = (event: React.FormEvent<HTMLTextAreaElement>) => {
+      const input = event.target as HTMLTextAreaElement;
+
+      input.style.height = "auto";
+      input.style.height = `${input.scrollHeight}px`;
+    };
+
+    return (
+      <Textarea
+        ref={ref}
+        rows={1}
+        className={cn(
+          "h-fit min-h-min resize-none overflow-hidden rounded-none border-0 p-0 focus-visible:ring-0",
+          className,
+        )}
+        onInput={handleInputGrow}
+        {...props}
+      />
+    );
+  },
+);
+AutoGrowInput.displayName = "AutoGrowInput";
+
+export default AutoGrowInput;
